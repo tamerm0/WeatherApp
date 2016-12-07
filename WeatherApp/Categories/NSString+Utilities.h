0466319d042c2671dd128ca233aceb7eca2e8dcc
Tamer <tamer.milad6@gmail.com>
2016-12-07 21:14:30 +0800

    Add NSString category and prefix header

---
+++ WeatherApp/Categories/NSString+Utilities.h
@@ -0,0 +1,18 @@
+//
+//  NSString+Utilities.h
+//  WeatherApp
+//
+//  Created by Top on 7/12/16.
+//  Copyright © 2016 Careem. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Utilities)
+
+/**
+ * Checks if string is empty or nil
+ */
++ (BOOL)isEmpty:(NSString *)string;
+
+@end
---
+++ WeatherApp/Categories/NSString+Utilities.m
@@ -0,0 +1,26 @@
+//
+//  NSString+Utilities.m
+//  WeatherApp
+//
+//  Created by Top on 7/12/16.
+//  Copyright © 2016 Careem. All rights reserved.
+//
+
+#import "NSString+Utilities.h"
+
+@implementation NSString (Utilities)
+
++ (BOOL)isEmpty:(NSString *)string {
+	if (string && [string isKindOfClass:[NSString class]]) {
+		
+		NSString *trimmedString = [string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+		if ([trimmedString length] == 0) {
+			
+			return YES;
+		} else {
+			return NO;
+		}
+	}
+	return YES;
+}
+@end
